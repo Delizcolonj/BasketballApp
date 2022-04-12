78281dd6be6781a18b4cabc05888e265aaac0de9
Crowleyj8atwit <55000966+Crowleyj8atwit@users.noreply.github.com>
2022-04-12 11:23:54 -0400

    WOP Ball Slide
    Note: Screen Parameters require finalization.

--- app/src/main/java/edu/wit/mobileapp/basketballapp/BallPhys.java
+++ app/src/main/java/edu/wit/mobileapp/basketballapp/BallPhys.java
@@ -1,8 +1,33 @@
 package edu.wit.mobileapp.basketballapp;
 
+import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
-public class BallPhys {
+public class BallPhys   {
     int x, y;
-    Bitmap fl;
+    int width;
+    int height;
+    Bitmap TheBall;
+    int moveDir;
+
+    BallPhys (int ScreenY, Resources Res){
+        TheBall = BitmapFactory.decodeResource(Res, R.drawable.ball);
+        height = TheBall.getHeight();
+        width = TheBall.getWidth();
+
+       // width /= 4;
+       // height /=4;
+
+
+        //TheBall = Bitmap.createScaledBitmap(TheBall, width, height);
+    }
+
+    Bitmap Movement (){
+        if (moveDir == 0) {
+            moveDir++;
+        }
+        moveDir--;
+        return TheBall;
+    }
 }
