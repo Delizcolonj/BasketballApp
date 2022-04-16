19d81372655a7f47c4571b646139cd2610c16f9c
Duoduo Xu <56886703+xudatwit@users.noreply.github.com>
2022-04-15 22:19:32 -0400

    Update Scoreboard.java

--- app/src/main/java/edu/wit/mobileapp/basketballapp/Scoreboard.java
+++ app/src/main/java/edu/wit/mobileapp/basketballapp/Scoreboard.java
@@ -1,102 +1,71 @@
 package edu.wit.mobileapp.basketballapp;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
 
 public class Scoreboard extends AppCompatActivity {
 
-    private String score;
+    private String databaseURL = "https://basketballapp-scoreboard-default-rtdb.firebaseio.com/";
 
     @Override
     protected void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
         setContentView(R.layout.score_list);
 
-        Button home_btn = (Button)findViewById(R.id.button4);
-
         ListView listView = findViewById(R.id.ListView01);
 
+        List<UserRecord> list = new ArrayList<>();
         Bitmap userIcon = BitmapFactory.decodeResource(getResources(), R.drawable.user_icon);
-
-        List<UserRecord> list = new ArrayList<UserRecord>();
-
-        writeFile();
-
-        score = readFile();
-        UserRecord record = new UserRecord(userIcon, score);
-        list.add(record);
-
-        Collections.sort(list, UserRecord.scoreAscending);
-        Collections.reverse(list);
-
         UserRecordAdapter adapter = new UserRecordAdapter(this, 0, list);
-        listView.setAdapter(adapter);
 
-        home_btn.setOnClickListener(new View.OnClickListener() {
+        //writeScore();
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance(databaseURL);
+        DatabaseReference myRef = database.getReference("score");
+
+        myRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.setClass(Scoreboard.this, MainActivity.class);
-                startActivity(intent);
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String score = dataSnapshot.getValue().toString();
+                score = score.replaceAll("[^?0-9]+", "");
+                List<String> scoreList = Arrays.asList(score.trim().split(""));
+                for (int i = 0; i < scoreList.size(); i++) {
+                    UserRecord record = new UserRecord(userIcon, scoreList.get(i));
+                    list.add(record);
+                }
+                adapter.notifyDataSetChanged();
+                Collections.sort(list, UserRecord.scoreAscending);
+                Collections.reverse(list);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
             }
         });
+        listView.setAdapter(adapter);
+
     }
 
-
-    private void writeFile() {
-        String score = "88";
-
-        try {
-            FileOutputStream fileOutputStream = openFileOutput("scoreHistory.txt", MODE_PRIVATE);
-            fileOutputStream.write(score.getBytes());
-            fileOutputStream.close();
-
-            Log.v("myApp", "score saved");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private void writeScore() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance(databaseURL);
+        DatabaseReference myRef = database.getReference("score");
+        String uuid = UUID.randomUUID().toString().replaceAll("[0-9]", "");
+        myRef.child("" + uuid).setValue(3);
     }
-
-    private String readFile() {
-        try {
-            FileInputStream fileInputStream = openFileInput("scoreHistory.txt");
-            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
-
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-            StringBuffer stringBuffer = new StringBuffer();
-
-            String lines;
-            while((lines = bufferedReader.readLine()) != null) {
-                stringBuffer.append(lines + "\n");
-            }
-            Log.v("myApp", "score = " + stringBuffer.toString());
-            return stringBuffer.toString();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
 }
